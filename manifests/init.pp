482f26511546067f26cf3a42bd955bced02f9417
Nathan O'Hanlon <nathan@eit.co.nz>
2014-08-26 21:37:10 +0100

    Latest

---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+class mysql::install($db_username, $db_password) {
+    package { 'mysql-server':
+        ensure => installed
+    }
+
+    service { 'mysqld':
+        ensure => 'running',
+        enable => true,
+        hasrestart => true,
+        hasstatus => true,
+        subscribe => Package['mysql-server'],
+    }
+
+    exec { 'create-db':
+        unless => "/usr/bin/mysql -u${db_username} -p${db_password}",
+        command => "/usr/bin/mysql -uroot -e \"GRANT ALL PRIVILEGES ON *.* TO ${db_username}@localhost IDENTIFIED BY '${db_password}';\"",
+        require => Service["mysqld"],
+    }
+}
+
+class mysql($db_username, $db_password) {
+    class { 'mysql::install':
+        db_username => $db_username,
+        db_password => $db_password
+    }
+}
