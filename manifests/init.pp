20ffd82ff97247d9f085c76b35b3ff83267f4b89
Nathan O'Hanlon <nathan@eit.co.nz>
2015-11-30 14:13:24 +0000

    Wordpress db

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,6 +18,28 @@ class mysql::install($db_username, $db_password) {
     }
 }
 
+class mysql::install-wordpress-db($wordpress_db_username, $wordpress_db_password) {
+    package { 'mysql-server':
+        ensure => installed
+    }
+
+    service { 'mysqld':
+        ensure => 'running',
+        enable => true,
+        hasrestart => true,
+        hasstatus => true,
+        subscribe => Package['mysql-server'],
+    }
+
+    exec { 'create-db':
+        unless => "/usr/bin/mysql -u${wordpress_db_username} -p${wordpress_db_password}",
+        command => "/usr/bin/mysql -uroot -e \"GRANT ALL PRIVILEGES ON *.* TO ${db_username}@localhost IDENTIFIED BY '${wordpress_db_password}';\"",
+        require => Service["mysqld"],
+    }
+
+}
+
+
 class mysql($db_username, $db_password) {
     class { 'mysql::install':
         db_username => $db_username,
